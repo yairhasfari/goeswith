1acaefb4f8381fcb1ad29833275b33cbb4aa431e
yairhasfari <yairhasfari@gmail.com>
2018-11-14 18:07:45 +0200

    added procfile

--- webweb2/settings.py
+++ webweb2/settings.py
@@ -15,6 +15,8 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -131,3 +133,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = (
     os.path.join(BASE_DIR,"static"),
 )
+
+django_heroku.settings(locals())
