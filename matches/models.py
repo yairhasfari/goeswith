9b9a18b4366f45b2ef713f257617cf007c93eb16
yairhasfari <yairhasfari@gmail.com>
2018-11-20 10:02:30 +0200

    Categories

--- matches/models.py
+++ matches/models.py
@@ -4,10 +4,13 @@ import matches
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural="Categories"
     def __str__(self):
         return self.name
 class Object(models.Model):
     name = models.CharField(max_length=100)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,default=None)
     image = models.CharField(max_length=1000)
     plural = models.BooleanField(default=False)
     def __str__(self):
